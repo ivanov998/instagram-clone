10275e29e98df32ef223f94a005cfc50bf2a4ab1
ivanov998 <quadcorebg@gmail.com>
2023-03-08 18:47:24 +0200

    Added random string generation and create post controller

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -1,11 +1,38 @@
 import { StatusCodes } from "http-status-codes";
 
+import { nanoid } from "nanoid";
+
+import { BadRequestError } from "../utils/errors.js";
+import db from "../utils/db.js";
+
 const createPost = async (req, res, next) => {
-    // Empty controller
-    res.sendStatus(StatusCodes.OK);
+
+    try {
+        let { caption } = req.body;
+        
+        if (!caption) {
+            throw new BadRequestError('Please provide all values');
+        }
+        
+        const { userId } = req;
+        const url = nanoid(12);
+
+        const { rows: insertedRows } = await db.query(`INSERT INTO posts (caption, url, user_id) VALUES ($1,$2,$3) RETURNING *;`,
+            [caption, url, userId]);
+
+        if (insertedRows.length) {
+            res.sendStatus(StatusCodes.OK);
+        } else {
+            throw new Error;
+        }
+
+    } catch (error) {
+        next(error);
+    }
 }
 
 const getPost = async (req, res, next) => {
+    console.log(req);
     // Empty controller
     res.sendStatus(StatusCodes.OK);
 }
