6f62bda72be33f71e175006975ff01820615192b
ivanov998 <quadcorebg@gmail.com>
2023-03-14 16:50:24 +0200

    Using react router link instead of standard link element in nav item

--- client/src/components/common/NavItem/index.js
+++ client/src/components/common/NavItem/index.js
@@ -1,14 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 
 import "./styles.css";
 
 const NavItem = ({icon, text, link, className, dropdown, onClick}) => {
   
-    const defaultClassNames = "nav-item d-block ";
+    const defaultClassNames = "nav-item d-block";
 
     let linkProps = {
-        href : link || "#", 
-        className: defaultClassNames + className,
+        to : link || "#", 
+        className: defaultClassNames.concat(" ", className),
         onClick
     }
 
@@ -24,12 +25,12 @@ const NavItem = ({icon, text, link, className, dropdown, onClick}) => {
     }
 
     return (
-        <a {...linkProps}>
+        <Link {...linkProps}>
             <span className="icon-wrapper me-xl-3 d-block d-xl-inline-block mx-auto">
                 <FontAwesomeIcon icon={icon} />
             </span>
             {text && <span className="label d-none d-xl-inline-block">{text}</span>}
-        </a>
+        </Link>
   );
 }
 
